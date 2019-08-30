343cdd14b91ecd0ab819f166b35c80e67588eabb
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2019-08-30 15:28:28 +0530

    Add files via upload

---
+++ book.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class book
+{
+string name;
+string author;
+string price;
+string num_pages;
+
+
+static book(string name,string author,string price,string num_pages)
+{
+this.name=name.
+this.author=author.
+this.price=price.
+this.num_pages=num_pages;
+}
+public String toString()
+{
+return("name of the book"+name+",name of the author"+author+",price of the book"+price+",number of pages in the book"+num_pages);
+}
+  }
+class demo
+{
+public  static void main(string org[])
+{
+book arr[]=new book[n];
+for(i=0;i<n;i++)
+{
+arr[i]=new book()
+}
+  }
+    }
+
+   
