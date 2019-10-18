ed86dfa09256d662404733fa1408e5b8ddafe49f
saihithesh123 <54038200+saihithesh123@users.noreply.github.com>
2019-10-18 08:49:20 +0530

    Update book.java

--- book.java
+++ book.java
@@ -1,34 +1,53 @@
 import java.util.*;
-class book
-{
-string name;
-string author;
-string price;
-string num_pages;
+class Bookdet{
+	String bname;
+	String auth;
+	int pr;
+	int pg;
+	void Set(String bname, String auth, int pr,int pg){
+		this.bname = bname;
+		this.auth = auth;
+		this.pr = pr;
+		this.pg = pg; 
+	}
 
 
-static book(string name,string author,string price,string num_pages)
-{
-this.name=name.
-this.author=author.
-this.price=price.
-this.num_pages=num_pages;
+	public String toString() {
+		String s1 = ("Book Name: "+ bname);
+		String s2 = ("Name of Author: "+ auth );
+		String s3 =  (" Price: "+ pr);
+		String s4 =  ("Number of pages: " + pg);
+		return (s1+s2+s3+s4);
+	}
 }
-public String toString()
-{
-return("name of the book"+name+",name of the author"+author+",price of the book"+price+",number of pages in the book"+num_pages);
+class Book{
+	public static void main(String args[]){
+		String bname;
+		String auth;
+		int pr;
+		int pg;
+		System.out.println("Enter the number of books");
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		Bookdet a[] = new Bookdet[n];
+		for (int i = 0;i<n;i++)
+		{
+			a[i] = new Book();
+			System.out.println("Enter the name of the book:");
+			bname = sc.next();
+			System.out.println("Enter the name of the Author:");
+			auth = sc.next();
+			System.out.println("Enter the price of the book:");
+			pr = sc.nextInt();
+			System.out.println("Enter the number of pages:");
+			pg = sc.nextInt();
+			a[i].Set(bname,auth,pr,pg);
+		}
+		System.out.println("Book Details are :");
+		for (i= 0;i<n;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
 }
-  }
-class demo
-{
-public  static void main(string org[])
-{
-book arr[]=new book[n];
-for(i=0;i<n;i++)
-{
-arr[i]=new book()
-}
-  }
-    }
-
    
